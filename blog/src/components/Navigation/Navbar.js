86353e406a392dc2169d676e9590a4cfc41aeb0c
ThienNTN <nguyentrannhatthien1710@gmail.com>
2022-10-12 00:10:32 +0700

    update user account

--- blog/src/components/Navigation/Navbar.js
+++ blog/src/components/Navigation/Navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import AdminNavbar from "./Admin/AdminNavbar";
+import PrivateNavbar from "./Private/PrivateNavbar";
 import PublicNavbar from "./Public/PublicNavbar";
 
 const Navbar = () => {
   //get user from store
   const state = useSelector(state => state.users);
   const { userAuth } = state;
-  const {data: currentUser} = userAuth;
+  const {data: currentUser} = userAuth || {};
+  const isAdmin = currentUser?.nhom_nhan_vien_id === '1';
+  console.log('userAuth', isAdmin );
 
   return (
     <>
-      {!!currentUser ? <AdminNavbar currentUser={currentUser} /> : <PublicNavbar />}
+      {isAdmin ? (
+        <AdminNavbar isLogin={currentUser} />
+      ) : currentUser ? (
+        <PrivateNavbar isLogin={currentUser} />
+      ) : (
+        <PublicNavbar />
+      )}
     </>
   );
 };
