9866b522e630c7c1ce87c025264a202493fa212f
ThienNTN <nguyentrannhatthien1710@gmail.com>
2022-10-27 17:49:55 +0700

    remove users manage repo

--- members-management/src/shared/utilities/validator.js
+++
@@ -1,9 +0,0 @@
-const validatePassword = (rule, value, callback) => {
-  if (value && value !== "Secret") {
-    callback("Error!");
-  } else {
-    callback();
-  }
-};
-
-export { validatePassword };
--- members-management/src/store/constant.js
+++
@@ -1,3 +0,0 @@
-export const ADD_MEMBER = "add_member";
-export const EDIT_MEMBER = "edit_member";
-export const DELETE_MEMBER = "delete_member";
